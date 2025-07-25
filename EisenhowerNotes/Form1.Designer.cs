00f1bf6a53a998821371c2e1442c07d2333d8c43
Ivan(SHLYASH) <YutaQ@list.ru>
2025-07-26 03:58:38 +0500

    ChangesWithBackgroundAndAddFirstLabel

--- EisenhowerNotes/Form1.Designer.cs
+++ EisenhowerNotes/Form1.Designer.cs
@@ -29,20 +29,41 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EisenhowerNotes));
+            this.mainFormName = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // mainFormName
+            // 
+            this.mainFormName.AutoSize = true;
+            this.mainFormName.BackColor = System.Drawing.Color.Transparent;
+            this.mainFormName.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.mainFormName.ForeColor = System.Drawing.SystemColors.Window;
+            this.mainFormName.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.mainFormName.Location = new System.Drawing.Point(204, 19);
+            this.mainFormName.MinimumSize = new System.Drawing.Size(500, 50);
+            this.mainFormName.Name = "mainFormName";
+            this.mainFormName.Size = new System.Drawing.Size(500, 50);
+            this.mainFormName.TabIndex = 0;
+            this.mainFormName.Text = "Eisenhower Notes v0.0";
+            this.mainFormName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // EisenhowerNotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(870, 452);
+            this.Controls.Add(this.mainFormName);
             this.Name = "EisenhowerNotes";
             this.Text = "Eisenhower Notes";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label mainFormName;
     }
 }
